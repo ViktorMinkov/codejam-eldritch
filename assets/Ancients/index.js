3de3f07aaceef0842b74b79cbd47fa37bc10dc6d
Viktor <minkovvs@gmail.com>
2022-08-26 18:43:21 +0300

    feat:update html and css

--- assets/Ancients/index.js
+++ assets/Ancients/index.js
@@ -1,13 +1,32 @@
-import azathoth from './Azathoth.png';
-import cthulhu from './Cthulthu.png';
-import iogSothoth from './IogSothoth.png';
-import shubNiggurath from './ShubNiggurath.png';
+// import azathoth from './Azathoth.png';
+// import cthulhu from './Cthulthu.png';
+// import iogSothoth from './IogSothoth.png';
+// import shubNiggurath from './ShubNiggurath.png';
 
-const ancients = {
-  azathoth,
-  cthulhu,
-  iogSothoth,
-  shubNiggurath
-}
+// const ancient = {
+//   azathoth,
+//   cthulhu,
+//   iogSothoth,
+//   shubNiggurath
+// }
+
+const ancients = [
+    {    
+      name: 'azathoth',
+      src: '../assets/Ancients/Azathoth.png',       
+    },  
+    {      
+      name: 'cthulhu',
+      src: '../assets/Ancients/Cthulthu.png',      
+    },
+    {      
+      name: 'iogSothoth',
+      src: '../assets/Ancients/IogSothoth.png',      
+    },
+    {      
+      name: 'shubNiggurath',
+      src: '../assets/Ancients/ShubNiggurath.png',      
+    },
+]
 
 export default ancients
---
+++ script.js
@@ -0,0 +1,8 @@
+import difficulties from './data/difficulties.js'
+import ancientsData from './data/ancients.js'
+
+import * as Cards from './data/mythicCards/index.js'
+// console.log(difficulties)
+console.log(ancientsData)
+console.log(Cards)
+
